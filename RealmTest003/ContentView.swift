f091cd0377c5922753154958a1e64c527325f957
Leejeeyong <fghj0720@gmail.com>
2019-10-17 13:04:15 +0900

    RealmTest init

---
+++ RealmTest003/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealmTest003
+//
+//  Created by GreenteaCat on 2019/10/17.
+//  Copyright © 2019 leejeeyong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
